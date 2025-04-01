2a1be3e368962140190bf32f80819fedf64acbc1
Pu Jiao <petejiao@outlook.com>
2025-04-01 16:08:44 -0400

    update

--- test_mpi/test_ssim_mpi.cpp
+++ test_mpi/test_ssim_mpi.cpp
@@ -58,7 +58,6 @@ int main(int argc, char** argv) {
     }
     // Create a 3D Cartesian communicator
     MPI_Cart_create(MPI_COMM_WORLD, 3, dims, periods, 1, &cart_comm);
-    // Get coordinates of this process
     MPI_Cart_coords(cart_comm, mpi_rank, 3, coords);
 
     // read data for each rank
@@ -67,7 +66,8 @@ int main(int argc, char** argv) {
     size_t num_elements = 0;
     auto data = readfile<float>(filename, num_elements);
 
-    sprintf(filename, "%s/%s_%d_%d_%d.decomp.f32", dir_prefix.c_str(), orig_prefix.c_str(), coords[0], coords[1], coords[2]);
+    sprintf(filename, "%s/%s_%d_%d_%d.decomp.f32", dir_prefix.c_str(), orig_prefix.c_str(), coords[0], coords[1],
+            coords[2]);
     size_t decomp_num_elements = 0;
     auto decomp_data = readfile<float>(filename, decomp_num_elements);
     // write the original data to one single file
@@ -77,30 +77,28 @@ int main(int argc, char** argv) {
     // use MPI_File_write_at to write the data to the file
     MPI_File fh;
     MPI_File fh_decomp;
-
     MPI_File_open(MPI_COMM_WORLD, out_filename.c_str(), MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
-    
     MPI_File_open(MPI_COMM_WORLD, decomp_filename.c_str(), MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL,
-    &fh_decomp);
-    
-    size_t global_offset_start = coords[0] * block_dims[0] * global_strides[0] +
-                                 coords[1] * block_dims[1] * global_strides[1] +
-                                 coords[2] * block_dims[2] * global_strides[2];
-
-    for (int bz = 0; bz < block_dims[0]; bz++) {
-        for (int by = 0; by < block_dims[1]; by++) {
-            size_t local_offset = bz * local_strides[0] + by * local_strides[1];
-            size_t cur_global_offset = global_offset_start + bz * global_strides[0] + by * global_strides[1];
-            size_t row_offset = cur_global_offset * sizeof(float);
-            MPI_File_write_at(fh, row_offset, &data[local_offset], block_dims[2], MPI_FLOAT, MPI_STATUS_IGNORE);
-            MPI_File_write_at(fh_decomp, row_offset, &decomp_data[local_offset], block_dims[2], MPI_FLOAT,
-                              MPI_STATUS_IGNORE);
+                  &fh_decomp);
+    // merge and write file to one file
+    {
+        int local_starts[3] = {0, 0, 0};
+        for (int i = 0; i < 3; i++) {
+            local_starts[i] = coords[i] * block_dims[i];
         }
+        MPI_Datatype subarray;
+        MPI_Type_create_subarray(3, orig_dims, block_dims, local_starts, MPI_ORDER_C, MPI_FLOAT, &subarray);
+        MPI_Type_commit(&subarray);
+        MPI_File_set_view(fh, 0, MPI_FLOAT, subarray, "native", MPI_INFO_NULL);
+        MPI_File_set_view(fh_decomp, 0, MPI_FLOAT, subarray, "native", MPI_INFO_NULL);
+        MPI_File_write_all(fh, data.get(), block_size, MPI_FLOAT, MPI_STATUS_IGNORE);
+        MPI_File_write_all(fh_decomp, decomp_data.get(), block_size, MPI_FLOAT, MPI_STATUS_IGNORE);
+        MPI_Type_free(&subarray);
+        MPI_File_close(&fh);
+        MPI_File_close(&fh_decomp);
     }
-    // barrier 
     MPI_Barrier(cart_comm);
-    MPI_File_close(&fh);
-    MPI_File_close(&fh_decomp);
+
 
     // prepare the a local buffer to read a larger size of the data,
     int ssim_win_size = 7;
@@ -114,25 +112,36 @@ int main(int argc, char** argv) {
         }
     }
     size_t w_block_size = w_block_dims[0] * w_block_dims[1] * w_block_dims[2];
-    size_t w_local_strides[3] = {w_block_dims[2] * w_block_dims[1], w_block_dims[2], 1}; 
+    size_t w_local_strides[3] = {w_block_dims[2] * w_block_dims[1], w_block_dims[2], 1};
     std::vector<float> w_orig_data(w_block_size, 0);
-    std::vector<float> w_decomp_data(w_block_size, 0); 
-    // read the data from the original merged and decompressed files 
-    MPI_File_open(MPI_COMM_WORLD, out_filename.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
-    MPI_File_open(MPI_COMM_WORLD, decomp_filename.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &fh_decomp);
-    for (int bz = 0; bz < w_block_dims[0]; bz++) {
-        for (int by = 0; by < w_block_dims[1]; by++) {
-            size_t local_offset = bz * w_local_strides[0] + by * w_local_strides[1];
-            size_t cur_global_offset = global_offset_start + bz * global_strides[0] + by * global_strides[1];
-            size_t row_offset = cur_global_offset * sizeof(float);
-            MPI_File_read_at(fh, row_offset, &w_orig_data[local_offset], w_block_dims[2], MPI_FLOAT,
-                             MPI_STATUS_IGNORE);
-            MPI_File_read_at(fh_decomp, row_offset, &w_decomp_data[local_offset], w_block_dims[2], MPI_FLOAT,
-                             MPI_STATUS_IGNORE);
+    std::vector<float> w_decomp_data(w_block_size, 0);
+    {
+        int local_starts[3] = {0, 0, 0};
+        for (int i = 0; i < 3; i++) {
+            local_starts[i] = coords[i] * block_dims[i];
         }
+
+        MPI_Datatype subarray;
+        MPI_Type_create_subarray(3, orig_dims, w_block_dims, local_starts, MPI_ORDER_C, MPI_FLOAT, &subarray);
+        MPI_Type_commit(&subarray);
+
+        // read the data from the original merged and decompressed files
+        MPI_File_open(MPI_COMM_WORLD, out_filename.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+        MPI_File_open(MPI_COMM_WORLD, decomp_filename.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &fh_decomp);
+
+        MPI_File_set_view(fh, 0, MPI_FLOAT, subarray, "native", MPI_INFO_NULL);
+        MPI_File_set_view(fh_decomp, 0, MPI_FLOAT, subarray, "native", MPI_INFO_NULL);
+
+        MPI_File_read_all(fh, w_orig_data.data(), w_block_size, MPI_FLOAT, MPI_STATUS_IGNORE);
+        MPI_File_read_all(fh_decomp, w_decomp_data.data(), w_block_size, MPI_FLOAT, MPI_STATUS_IGNORE);
+        MPI_Type_free(&subarray);
+        // free the data
+        MPI_File_close(&fh);
+        MPI_File_close(&fh_decomp);
     }
-    double local_ssim_sum =0;
-    double local_nw = 0; 
+
+    double local_ssim_sum = 0;
+    double local_nw = 0;
     size_t max_offset2 = w_block_dims[0] - ssim_win_size;
     size_t max_offset1 = w_block_dims[1] - ssim_win_size;
     size_t max_offset0 = w_block_dims[2] - ssim_win_size;
@@ -140,11 +149,9 @@ int main(int argc, char** argv) {
         for (size_t offset1 = 0; offset1 <= max_offset1; offset1 += ssim_win_shift) {
             for (size_t offset0 = 0; offset0 <= max_offset0; offset0 += ssim_win_shift) {
                 local_nw++;
-                local_ssim_sum += PM::SSIM_3d_calcWindow(w_orig_data.data(), w_decomp_data.data(), 
-                                                w_block_dims[1], w_block_dims[2],
-                                                offset0, offset1, offset2, 
-                                                ssim_win_size, ssim_win_size,
-                                                        ssim_win_size);
+                local_ssim_sum +=
+                    PM::SSIM_3d_calcWindow(w_orig_data.data(), w_decomp_data.data(), w_block_dims[1], w_block_dims[2],
+                                           offset0, offset1, offset2, ssim_win_size, ssim_win_size, ssim_win_size);
             }
         }
     }
@@ -155,10 +162,6 @@ int main(int argc, char** argv) {
     if (mpi_rank == 0) {
         printf("SSIM: %f\n", global_ssim_sum / global_nw);
     }
-    // free the data
-    MPI_File_close(&fh);
-    MPI_File_close(&fh_decomp);
-
 
     MPI_Comm_free(&cart_comm);
     MPI_Finalize();
--- test_mpi/test_ssim_mpi_merged_file.cpp
+++ test_mpi/test_ssim_mpi_merged_file.cpp
@@ -26,8 +26,8 @@ int main(int argc, char** argv) {
     dims[0] = atoi(argv[1]);
     dims[1] = atoi(argv[2]);
     dims[2] = atoi(argv[3]);
-    std::string orig_file = argv[4];     // Directory prefix for the blocks
-    std::string decomp_filename = argv[5];    // Name prefix for the blocks
+    std::string orig_file = argv[4];        // Directory prefix for the blocks
+    std::string decomp_filename = argv[5];  // Name prefix for the blocks
 
     int orig_dims[3] = {256, 384, 384};
     orig_dims[0] = atoi(argv[6]);
@@ -40,8 +40,8 @@ int main(int argc, char** argv) {
     block_dims[2] = orig_dims[2] / dims[2];
     size_t block_size = block_dims[0] * block_dims[1] * block_dims[2];
 
-    size_t global_strides[3] = {orig_dims[2] * orig_dims[1], orig_dims[2], 1};
-    size_t local_strides[3] = {block_dims[2] * block_dims[1], block_dims[2], 1};
+    size_t global_strides[3] = {(size_t)orig_dims[2] * orig_dims[1], (size_t)orig_dims[2], 1};
+    size_t local_strides[3] = {(size_t)block_dims[2] * block_dims[1], (size_t)block_dims[2], 1};
 
     int periods[3] = {0, 0, 0};  // No periodicity in any dimension
     int coords[3] = {0, 0, 0};   // Coords of this process in the grid
@@ -60,10 +60,11 @@ int main(int argc, char** argv) {
     // Get coordinates of this process
     MPI_Cart_coords(cart_comm, mpi_rank, 3, coords);
 
-    // sprintf(filename, "%s/write_%d_%d_%d.f33", dir_prefix.c_str(), coords[0], coords[1], coords[2]);
-    // use MPI_File_write_at to write the data to the file
-    MPI_File fh;
-    MPI_File fh_decomp;
+    int local_starts[3] = {0, 0, 0};
+    for (int i = 0; i < 3; i++) {
+        local_starts[i] = coords[i] * block_dims[i];
+    }
+
     size_t global_offset_start = coords[0] * block_dims[0] * global_strides[0] +
                                  coords[1] * block_dims[1] * global_strides[1] +
                                  coords[2] * block_dims[2] * global_strides[2];
@@ -80,39 +81,46 @@ int main(int argc, char** argv) {
         }
     }
     size_t w_block_size = w_block_dims[0] * w_block_dims[1] * w_block_dims[2];
-    size_t w_local_strides[3] = {w_block_dims[2] * w_block_dims[1], w_block_dims[2], 1}; 
+    size_t w_local_strides[3] = {(size_t)w_block_dims[2] * w_block_dims[1], (size_t)w_block_dims[2], 1};
     std::vector<float> w_orig_data(w_block_size, 0);
-    std::vector<float> w_decomp_data(w_block_size, 0); 
-    // read the data from the original merged and decompressed files 
+    std::vector<float> w_decomp_data(w_block_size, 0);
+
+    MPI_Datatype subarray;
+    MPI_Type_create_subarray(3, orig_dims, w_block_dims, local_starts, MPI_ORDER_C, MPI_FLOAT, &subarray);
+    MPI_Type_commit(&subarray);
+
+    MPI_File fh;
+    MPI_File fh_decomp;
+
+    // read the data from the original merged and decompressed files
     MPI_File_open(MPI_COMM_WORLD, orig_file.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
     MPI_File_open(MPI_COMM_WORLD, decomp_filename.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &fh_decomp);
-    for (int bz = 0; bz < w_block_dims[0]; bz++) {
-        for (int by = 0; by < w_block_dims[1]; by++) {
-            size_t local_offset = bz * w_local_strides[0] + by * w_local_strides[1];
-            size_t cur_global_offset = global_offset_start + bz * global_strides[0] + by * global_strides[1];
-            size_t row_offset = cur_global_offset * sizeof(float);
-            MPI_File_read_at(fh, row_offset, &w_orig_data[local_offset], w_block_dims[2], MPI_FLOAT,
-                             MPI_STATUS_IGNORE);
-            MPI_File_read_at(fh_decomp, row_offset, &w_decomp_data[local_offset], w_block_dims[2], MPI_FLOAT,
-                             MPI_STATUS_IGNORE);
-        }
+
+    MPI_File_set_view(fh, 0, MPI_FLOAT, subarray, "native", MPI_INFO_NULL);
+    MPI_File_set_view(fh_decomp, 0, MPI_FLOAT, subarray, "native", MPI_INFO_NULL);
+
+    double read_time = MPI_Wtime();
+    MPI_File_read_all(fh, w_orig_data.data(), w_block_size, MPI_FLOAT, MPI_STATUS_IGNORE);
+    MPI_File_read_all(fh_decomp, w_decomp_data.data(), w_block_size, MPI_FLOAT, MPI_STATUS_IGNORE);
+    double read_time_end = MPI_Wtime();
+    if (mpi_rank == 0) {
+        printf("Read time: %f\n", read_time_end - read_time);
     }
-    double local_ssim_sum =0;
-    double local_nw = 0; 
+
+    double local_ssim_sum = 0;
+    double local_nw = 0;
     size_t max_offset2 = w_block_dims[0] - ssim_win_size;
     size_t max_offset1 = w_block_dims[1] - ssim_win_size;
     size_t max_offset0 = w_block_dims[2] - ssim_win_size;
 
-    double time = MPI_Wtime(); 
+    double time = MPI_Wtime();
     for (size_t offset2 = 0; offset2 <= max_offset2; offset2 += ssim_win_shift) {
         for (size_t offset1 = 0; offset1 <= max_offset1; offset1 += ssim_win_shift) {
             for (size_t offset0 = 0; offset0 <= max_offset0; offset0 += ssim_win_shift) {
                 local_nw++;
-                local_ssim_sum += PM::SSIM_3d_calcWindow(w_orig_data.data(), w_decomp_data.data(), 
-                                                w_block_dims[1], w_block_dims[2],
-                                                offset0, offset1, offset2, 
-                                                ssim_win_size, ssim_win_size,
-                                                        ssim_win_size);
+                local_ssim_sum +=
+                    PM::SSIM_3d_calcWindow(w_orig_data.data(), w_decomp_data.data(), w_block_dims[1], w_block_dims[2],
+                                           offset0, offset1, offset2, ssim_win_size, ssim_win_size, ssim_win_size);
             }
         }
     }
@@ -121,7 +129,7 @@ int main(int argc, char** argv) {
     MPI_Reduce(&local_ssim_sum, &global_ssim_sum, 1, MPI_DOUBLE, MPI_SUM, 0, cart_comm);
     MPI_Reduce(&local_nw, &global_nw, 1, MPI_DOUBLE, MPI_SUM, 0, cart_comm);
     if (mpi_rank == 0) {
-        printf("SSIM: %f\n", global_ssim_sum / global_nw);
+        printf("SSIM =  %.6f\n", global_ssim_sum / global_nw);
     }
     double time_end = MPI_Wtime();
     if (mpi_rank == 0) {
@@ -130,7 +138,6 @@ int main(int argc, char** argv) {
     // free the data
     MPI_File_close(&fh);
     MPI_File_close(&fh_decomp);
-
 
     MPI_Comm_free(&cart_comm);
     MPI_Finalize();
